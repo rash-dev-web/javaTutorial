b9ccfbfaf27f1ddcc6f31ef1ffe5faf95fe1a85f
Rasheed Mohammad <rasheed.m@live.com>
2022-01-10 21:46:15 +0530

    variable declaration

---
+++ src/test/java/section3/CurrencyConverter.java
@@ -0,0 +1,35 @@
+package section3;
+
+public class CurrencyConverter {
+
+	int rupee = 75;
+	int dirham = 3;
+	int real = 3;
+	int chilean_peso = 595;
+	int mexican_peso = 18;
+	int _yen = 107;
+	int $australian = 2;
+	int dollar;
+	int Rupee = 63;
+
+	void printCurrencies() {
+
+		System.out.println("rupee: " + rupee);
+		System.out.println("dirhan: " + dirham);
+		System.out.println("real: " + real);
+		System.out.println("chilean_peso: " + chilean_peso);
+		System.out.println("mexican_peso: " + mexican_peso);
+		System.out.println("_yen: " + _yen);
+		System.out.println("dollar: " + dollar);
+		System.out.println("Rupee: " + Rupee);
+		System.out.println("$australian: " + $australian);
+
+	}
+	
+	public static void main(String[] args) {
+		
+		CurrencyConverter cc = new CurrencyConverter();
+		cc.printCurrencies();
+	}
+
+}
