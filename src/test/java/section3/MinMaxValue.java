335894f014090211631fcf4594c6a82d2151cc8c
Rasheed Mohammad <rasheed.m@live.com>
2022-01-10 23:27:17 +0530

    MIN and MAX constants

---
+++ src/test/java/section3/MinMaxValue.java
@@ -0,0 +1,28 @@
+package section3;
+
+public class MinMaxValue {
+	
+	public static void main(String[] args) {
+		
+		byte minValue = Byte.MIN_VALUE;
+		byte maxValue = Byte.MAX_VALUE;
+		System.out.println("byte minValue: "+ minValue);
+		System.out.println("byte maxValue: "+ maxValue);
+		
+		short sminValue = Short.MIN_VALUE;
+		short smaxValue = Short.MAX_VALUE;
+		System.out.println("short minValue: "+ sminValue);
+		System.out.println("short maxValue: "+ smaxValue);
+		
+		int iminValue = Integer.MIN_VALUE;
+		int imaxValue = Integer.MAX_VALUE;
+		System.out.println("int minValue: "+ iminValue);
+		System.out.println("int maxValue: "+ imaxValue);
+		
+		long lminValue = Long.MIN_VALUE;
+		long lmaxValue = Long.MAX_VALUE;
+		System.out.println("long minValue: "+ lminValue);
+		System.out.println("long maxValue: "+ lmaxValue);
+	}
+
+}
