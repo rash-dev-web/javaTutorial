e88513a153f0dc8ea6c4b2d422036744f142797a
Rasheed Mohammad <rasheed.m@live.com>
2022-01-14 19:04:40 +0530

    add type casting demo

---
+++ src/test/java/section3/TypeCasting.java
@@ -0,0 +1,31 @@
+package section3;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+
+		// Implicit - smaller to larger - widening
+		int x = 65;
+		long y = x;
+		System.out.println("y: " + y);
+
+		int a = 13;
+		float b = a;
+		System.out.println("b: " + b);
+		
+		// Explicit - larger to smaller - narrowing
+		long c = 64;
+		int d = (int)c;
+		System.out.println("d: " + d);
+		
+		byte e = 65;
+		char f = (char)e;
+		System.out.println("f: " + f);
+		
+		char g = 65;
+		System.out.println("g: " + g);
+		
+		short h = 'A';
+		System.out.println("h: " + h);
+	}
+}
