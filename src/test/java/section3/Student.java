150c79524fe040cc882223f12c38395d307fc404
Rasheed Mohammad <rasheed.m@live.com>
2022-01-18 19:55:18 +0530

    add Student class updated code

--- src/test/java/section3/Student.java
+++ src/test/java/section3/Student.java
@@ -1,31 +1,57 @@
 package section3;
 
 public class Student {
-	
-	static int studenCount;
+
+	static int studentCount;
 	int id;
 	String name;
 	String gender;
 	int age;
 	long phone;
-	long gpa;
+	double gpa;
 	char degree;
+
 	boolean international;
-	
-	
-	double tutionFee = 10000;
-	double internationalFee = 5000;
-	
-	Student(){
-		
+	double tutionFee = 10000.0;
+	double internationalFee = 5000.0;
+
+	Student() {
+
 	}
-	
-	public Student(int newId, String newName) {
-		id = newId;
-		name = newName;
+
+	public Student(int id, String name, String gender, int age, long phone, double gpa, char degree) {
+		this(id, name, gender, age, phone, gpa, degree, false);
+	}
+
+	public Student(int id, String name, String gender, int age, long phone, double gpa, char degree,
+			boolean international) {
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.phone = phone;
+		this.gpa = gpa;
+		this.degree = degree;
+		this.international = international;
+
+		studentCount = studentCount + 1;
+
+		if (international) {
+			tutionFee = tutionFee + internationalFee;
+		}
+
+		System.out.println("Id: " + this.id);
+		System.out.println("Name: " + this.name);
+		System.out.println("Gender: " + this.gender);
+		System.out.println("Age: " + this.age);
+		System.out.println("Phone: " + this.phone);
+		System.out.println("GPA: " + this.gpa);
+		System.out.println("Degree: " + this.degree);
+
 	}
 
 	public static void main(String[] args) {
-		
+
+		Student st1 = new Student(1, "Mir", "M", 21, 8765432109l, 3.4, 'A');
 	}
 }
